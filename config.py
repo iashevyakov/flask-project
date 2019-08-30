ce2e0e86911699474109343029d542a3ca7a5547
ivan <iashevyakov@mail.ru>
2019-08-30 14:37:50 +0200

    setting nginx, uwsgi, postgresql, redis on docker containers

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
     SECRET_KEY = 'bwt35sQ'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://taskrole:taskrole77@localhost:5432/task'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://taskrole:taskrole77@db:5432/task'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = 'localhost'
+    REDIS_HOST = 'redis'
     REDIS_PORT = '6379'
     REDIS_DB = 0
